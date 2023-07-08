83e69097392d286d1c8b4897963e0f01b43fad61
Pabitra Kumar Bebartta <pabitrakumarbebartta@gmail.com>
2023-07-08 21:49:31 +0530

    8 july

--- POTDS/MergeWithoutExtraSpace.java
+++ POTDS/MergeWithoutExtraSpace.java
@@ -2,32 +2,31 @@ import java.util.Arrays;
 
 public class MergeWithoutExtraSpace {
     public static void main(String[] args) {
-        long arr1[] = {1, 3, 5, 7};
-        long arr2[] = {0, 2, 6, 8, 9};
+        long arr1[] = { 1, 3, 5, 7 };
+        long arr2[] = { 0, 2, 6, 8, 9 };
         merge(arr1, arr2, arr1.length, arr2.length);
-        System.out.println(Arrays.toString(arr1)+'\n'+Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr1) + '\n' + Arrays.toString(arr2));
     }
-    public static void merge(long arr1[], long arr2[], int n, int m) 
-    {
-        int i = n-1;
+
+    public static void merge(long arr1[], long arr2[], int n, int m) {
+        int i = n - 1;
         int j = 0;
-        while(i>=0 && j<m){
-                
-                if(arr1[i]>arr2[j]){
-                    
-                    swap(arr1, i, arr2, j);
-                    i--;
-                    j++;
-                }
-                else{
-                    break;
-                }
+        while (i >= 0 && j < m) {
+
+            if (arr1[i] > arr2[j]) {
+
+                swap(arr1, i, arr2, j);
+                i--;
+                j++;
+            } else {
+                break;
             }
-            Arrays.sort(arr1);
-            Arrays.sort(arr2);
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
     }
-    static void swap(long[] arr1, int i, long[] arr2, int j)
-    {
+
+    static void swap(long[] arr1, int i, long[] arr2, int j) {
         long temp = arr1[i];
         arr1[i] = arr2[j];
         arr2[j] = temp;
